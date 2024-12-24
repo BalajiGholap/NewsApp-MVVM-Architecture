2d75c79e917a2cb6772599a5321aa4b165848bf5
bg00781907 <balagholap777@gmail.com>
2024-12-24 10:06:38 +0530

    Removed unused files and code

--- app/src/main/java/com/bala/newsapp_mvvm_architecture/model/Cars.kt
+++
@@ -1,9 +0,0 @@
-package com.bala.newsapp_mvvm_architecture.model
-
-data class Cars(
-    val carname: String,
-    val id: String,
-    val image: String,
-    val model: String,
-    val price: Int
-)
--- app/src/main/java/com/bala/newsapp_mvvm_architecture/retrofit/NewsApi.kt
+++ app/src/main/java/com/bala/newsapp_mvvm_architecture/retrofit/NewsApi.kt
@@ -1,6 +1,5 @@
 package com.bala.newsapp_mvvm_architecture.retrofit
 
-import com.bala.newsapp_mvvm_architecture.model.Cars
 import com.bala.newsapp_mvvm_architecture.model.topheadlines.NewsData
 import com.bala.newsapp_mvvm_architecture.model.sources.Sources
 import com.bala.newsapp_mvvm_architecture.utils.Constants
@@ -9,9 +8,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApi {
-
-    @GET("cars")
-    suspend fun getCars(): Response<List<Cars>>
 
     @GET("v2/top-headlines")
     suspend fun getTopHeadlines(
